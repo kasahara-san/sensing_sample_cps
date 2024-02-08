aef31f8cf864a0850a493cbc8061fdca0e4d3a97
kasahara-san <kasahara.yuichiro.res@gmail.com>
2024-02-08 15:18:23 +0900

    Update: トピック名・データ型を一部変更

--- sample_sensing_nodes/src/zx200_end_effector_publisher.cpp
+++ sample_sensing_nodes/src/zx200_end_effector_publisher.cpp
@@ -5,34 +5,34 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "sensing_msgs/msg/zx120_end_effector.hpp"
+#include "sensing_msgs/msg/zx200_end_effector.hpp"
 
 using namespace std::chrono_literals;
 
 
-class Zx120EndEffectorPublisher : public rclcpp::Node
+class Zx200EndEffectorPublisher : public rclcpp::Node
 {
   public:
-    Zx120EndEffectorPublisher()
+    Zx200EndEffectorPublisher()
     : Node("simple_publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<sensing_msgs::msg::Zx120EndEffector>("/zx120/end_effector", 10);
-      timer_ = this->create_wall_timer(1000ms, std::bind(&Zx120EndEffectorPublisher::timer_callback, this));
+      publisher_ = this->create_publisher<sensing_msgs::msg::Zx200EndEffector>("/zx200/end_effector", 10);
+      timer_ = this->create_wall_timer(1000ms, std::bind(&Zx200EndEffectorPublisher::timer_callback, this));
     }
 
   private:
     void timer_callback()
     {
-      auto message = sensing_msgs::msg::Zx120EndEffector();
+      auto message = sensing_msgs::msg::Zx200EndEffector();
       message.model_name = "zx200";
-      message.component_name = "end_effector";
+      message.record_name = "end_effector";
       message.position_with_angle = 1;
       message.x = x;
       message.y = y;
       message.z = z;
       message.theta_w = theta_w;
       y += 0.1;
-      RCLCPP_INFO(this->get_logger(), "Sending the Zx120EndEffector.msg to parameter( model_name = %s,component_name : %s) in mongodb", message.model_name.c_str(),message.component_name.c_str());
+      RCLCPP_INFO(this->get_logger(), "Sending the Zx200EndEffector.msg to parameter( model_name = %s,record_name : %s) in mongodb", message.model_name.c_str(),message.record_name.c_str());
       RCLCPP_INFO(this->get_logger(), "x : %f", message.x);
       RCLCPP_INFO(this->get_logger(), "y : %f", message.y);
       RCLCPP_INFO(this->get_logger(), "z : %f", message.z);
@@ -41,7 +41,7 @@ class Zx120EndEffectorPublisher : public rclcpp::Node
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<sensing_msgs::msg::Zx120EndEffector>::SharedPtr publisher_;
+    rclcpp::Publisher<sensing_msgs::msg::Zx200EndEffector>::SharedPtr publisher_;
     size_t count_;
     float x=0.0;
     float y=0.0;
@@ -52,7 +52,7 @@ class Zx120EndEffectorPublisher : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Zx120EndEffectorPublisher>());
+  rclcpp::spin(std::make_shared<Zx200EndEffectorPublisher>());
   rclcpp::shutdown();
   return 0;
 }
